4e5de7aa920c99e4fdcb9bfa77c3fe89b4ad2f8b
VenCz <viniciuscesarp3@gmail.com>
2025-08-24 20:10:24 -0300

    Improving the code

--- PokeAPISevenDaysOfCode/Model/PokemonListResponse.cs
+++ PokeAPISevenDaysOfCode/Model/PokemonListResponse.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PokeAPISevenDaysOfCode.Model;
+﻿using PokeAPISevenDaysOfCode.Model;
 
 public class PokemonListResponse
 {
     public int Count { get; set; }
     public string? Next { get; set; }
     public string? Previous { get; set; }
-
     public List<Pokemon> Results { get; set; } = new();
 }
--- PokeAPISevenDaysOfCode/Program.cs
+++ PokeAPISevenDaysOfCode/Program.cs
@@ -1,12 +1,10 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using PokeAPISevenDaysOfCode.Services;
+﻿using PokeAPISevenDaysOfCode.Services;
 
-// Remove the local Main method and use top-level statements for the entry point
 var client = new HttpClient();
 
 var pokeClient = new PokeApiClient(client);
 
-// Await the asynchronous method to get the result
+// Espera o método asincrono para trazer os pokemons
 var page = await pokeClient.GetPokemonAsync(50, 0);
 
 foreach (var pokemon in page.Results)
