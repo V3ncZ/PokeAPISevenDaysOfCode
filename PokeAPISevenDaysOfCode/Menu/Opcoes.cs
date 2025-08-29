61a44c2930a5196882fc032bebae3dc6819d4426
VenCz <viniciuscesarp3@gmail.com>
2025-08-28 22:35:53 -0300

    Implementando desafio do dia 5 - Interacoes com pokemon adicionadas

--- PokeAPISevenDaysOfCode/Menu/Opcoes.cs
+++ PokeAPISevenDaysOfCode/Menu/Opcoes.cs
@@ -1,57 +1,121 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using PokeAPISevenDaysOfCode.Model;
 using PokeAPISevenDaysOfCode.Services;
 
 namespace PokeAPISevenDaysOfCode.Menu
 {
     public class Opcoes
     {
+
+        public string NomeJogador { get; set; }
+
+        public string EspecieEscolhida { get; set; }
+
+        public Pokemon Pokemon { get; set; }
+
+        public List<Pokemon> ListaDePokemons { get; set; }
+
         public async Task MenuOpcoes()
         {
-            Console.WriteLine(@" _                                    _       _     _ 
-| |                                  | |     | |   (_)
-| |_ __ _ _ __ ___   __ _  __ _  ___ | |_ ___| |__  _ 
-| __/ _` | '_ ` _ \ / _` |/ _` |/ _ \| __/ __| '_ \| |
-| || (_| | | | | | | (_| | (_| | (_) | || (__| | | | |
- \__\__,_|_| |_| |_|\__,_|\__, |\___/ \__\___|_| |_|_|
-                           __/ |                      
-                          |___/                  ");
+
+            Console.Clear();
 
             MensagemDeBoasVindas();
 
-            Console.WriteLine("");
-
-            Console.WriteLine("-----------------------------------------");
-            Console.WriteLine("------------------- MENU ----------------");
-            Console.WriteLine("-----------------------------------------");
-
-            Console.WriteLine("");
-
-            Console.WriteLine("1 - ADOTAR MASCOTE VIRTUAL");
-            Console.WriteLine("2 - VER SEUS MASCOTES");
-            Console.WriteLine("3 - SAIR");
-
-            Console.WriteLine("ESCOLHA UMA OPCAO: ");
-            var keyInfo = Console.ReadKey();
-            int opcao = keyInfo.KeyChar - '0';
-
-            switch (opcao)
+            bool sair = false;
+            while (!sair)
             {
-                case 1:
-                    await AdotarUmMascote();
-                    break;
-                case 2:
-                    VerSeusMascotes();
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
+
+                Console.WriteLine("\n-----------------------------------------");
+                Console.WriteLine("------------------- MENU ----------------");
+                Console.WriteLine("-----------------------------------------");
+
+                Console.WriteLine("1 - ADOTAR MASCOTE VIRTUAL");
+                Console.WriteLine("2 - VER SEUS MASCOTES");
+                Console.WriteLine("3 - INTERAGIR COM SEU MASCOTE");
+                Console.WriteLine("4 - SAIR");
+
+                Console.WriteLine($"\n{NomeJogador} ESCOLHA UMA OPCAO....");
+                var keyInfo = Console.ReadKey(true);
+                int opcao = keyInfo.KeyChar - '0';
+
+                switch (opcao)
+                {
+                    case 1:
+                        await AdotarUmMascote();
+                        break;
+                    case 2:
+                        VerSeusMascotes();
+                        break;
+                    case 3:
+                        InteragirComMascote();
+                        break;
+                    case 4:
+                        Console.WriteLine("NOS VEMOS LOGO! ATE MAIS!");
+                        sair = true;
+                        break;
+                    default:
+                        break;
+                }
             }
+
+            
+        }
+
+        private void InteragirComMascote()
+        {
+
+            Console.Clear();
+
+            bool voltar = false;
+            while (!voltar)
+            {
+
+                Console.WriteLine("\n-----------------------------------------");
+                Console.WriteLine($"{NomeJogador} VOCE DESEJA....");
+                Console.WriteLine($"1 - SABER COMO {EspecieEscolhida} ESTA");
+                Console.WriteLine($"2 - ALIMENTAR {EspecieEscolhida}");
+                Console.WriteLine($"3 - BRINCAR COM {EspecieEscolhida}");
+                Console.WriteLine("4 - VOLTAR");
+                var keyInfo = Console.ReadKey(true);
+                int opcao = keyInfo.KeyChar - '0';
+
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("\n-----------------------------------------");
+                        Pokemon.StatusDoPokemon();
+                        break;
+                    case 2:
+                        Console.WriteLine("\n-----------------------------------------");
+                        Pokemon.Alimentar();
+                        Console.WriteLine($"{Pokemon.Name.ToUpper()} ALIMENTADO!");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n-----------------------------------------");
+                        Pokemon.Brincar();
+                        Console.WriteLine($"{Pokemon.Name.ToUpper()} ESTA MAIS FELIZ!");
+                        break;
+                    case 4:
+                        voltar = true;
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        Console.WriteLine("OPCAO INVALIDA VOLTANDO PARA O MENU DE OPCOES...");
+                        voltar = true;
+                        break;
+
+                }
+            }
+
+            
         }
 
         private void VerSeusMascotes()
@@ -61,6 +125,8 @@ namespace PokeAPISevenDaysOfCode.Menu
 
         private async Task AdotarUmMascote()
         {
+            Console.Clear();
+
             Console.WriteLine("");
 
             Console.WriteLine("-----------------------------------------");
@@ -79,40 +145,44 @@ namespace PokeAPISevenDaysOfCode.Menu
                 Console.WriteLine(pokemon.Name.ToUpper());
             }
 
-            Console.WriteLine("\nESCOLHA UMA ESPECIE: ");
-            string especieEscolhida = Console.ReadLine()!;
+            Console.WriteLine($"\n{NomeJogador} ESCOLHA UMA ESPECIE: ");
+            EspecieEscolhida = Console.ReadLine().ToUpper()!;
+
+            var pokemonDetails = await pokeClient.GetSpecificPokemon(EspecieEscolhida);
+
+            Pokemon = new Pokemon(pokemonDetails.Name, pokemonDetails.Height, pokemonDetails.Weight, pokemonDetails.Abilities);
+
+            ListaDePokemons.Add(Pokemon);
 
             bool voltar = false;
-
             while (!voltar)
             {
 
                 Console.WriteLine("\n-----------------------------------------");
-                Console.WriteLine($"1 - SABER MAIS SOBRE O {especieEscolhida}");
-                Console.WriteLine($"2 - ADOTAR O {especieEscolhida}");
+                Console.WriteLine($"1 - SABER MAIS SOBRE O {Pokemon.Name.ToUpper()}");
+                Console.WriteLine($"2 - ADOTAR O {Pokemon.Name.ToUpper()}");
                 Console.WriteLine("3 - VOLTAR");
-                Console.WriteLine("\nESCOLHA UMA OPCAO:");
+                Console.WriteLine($"\n{NomeJogador} ESCOLHA UMA OPCAO...");
                 var keyInfo = Console.ReadKey();
                 int opcao = keyInfo.KeyChar - '0';
 
                 switch (opcao)
                 {
                     case 1:
-                        var pokemonDetails = await pokeClient.GetSpecificPokemon(especieEscolhida);
-
                         Console.WriteLine("\n-----------------------------------------");
-                        Console.WriteLine($"NOME: {pokemonDetails.Name}");
-                        Console.WriteLine($"ALTURA: {pokemonDetails.Height}");
-                        Console.WriteLine($"PESO: {pokemonDetails.Weight}");
+                        Console.WriteLine($"NOME: {Pokemon.Name.ToUpper()}");
+                        Console.WriteLine($"ALTURA: {Pokemon.Height}");
+                        Console.WriteLine($"PESO: {Pokemon.Weight}");
                         Console.WriteLine("HABILIDADES:");
 
-                        foreach (var ability in pokemonDetails.Abilities)
+                        foreach (var ability in Pokemon.Abilities)
                         {
                             Console.WriteLine($"{ability.Ability.Name}");
                         }
                         break;
                     case 2:
-                        Console.WriteLine($"\n{especieEscolhida} FOI ESCOLHIDO COMO SEU NOVO MASCOTE, O OVO ESTA CHOCANDO!");
+                        Console.Clear();
+                        Console.WriteLine($"\n{Pokemon.Name.ToUpper()} FOI ESCOLHIDO COMO SEU NOVO MASCOTE, O OVO ESTA CHOCANDO!");
                         Console.WriteLine(@"                                                                          
                                                                           
                                                                           
@@ -136,6 +206,10 @@ namespace PokeAPISevenDaysOfCode.Menu
                                                                           
                                                                           
 ");
+                        Thread.Sleep(5000);
+                        Console.WriteLine($"O OVO CHOCOU!\n{Pokemon.Name.ToUpper()} AGORA É SEU NOVO MASCOTE!");
+                        Console.WriteLine("\nPRESSIONE QUALQUER TECLA PARA VOLTAR....");
+                        Console.ReadKey();
                         voltar = true;
                         break;
                     case 3:
@@ -147,13 +221,25 @@ namespace PokeAPISevenDaysOfCode.Menu
             }
         }
 
-
         public void MensagemDeBoasVindas()
         {
-            Console.WriteLine("DIGITE O SEU NOME: ");
-            string nome = Console.ReadLine()!;
+            Console.WriteLine(@" _                                    _       _     _ 
+| |                                  | |     | |   (_)
+| |_ __ _ _ __ ___   __ _  __ _  ___ | |_ ___| |__  _ 
+| __/ _` | '_ ` _ \ / _` |/ _` |/ _ \| __/ __| '_ \| |
+| || (_| | | | | | | (_| | (_| | (_) | || (__| | | | |
+ \__\__,_|_| |_| |_|\__,_|\__, |\___/ \__\___|_| |_|_|
+                           __/ |                      
+                          |___/                  ");
 
-            Console.WriteLine($"\nBEM VINDO {nome}");
+            if (string.IsNullOrEmpty(NomeJogador))
+            {
+                Console.WriteLine("DIGITE O SEU NOME: ");
+
+                NomeJogador = Console.ReadLine().ToUpper();
+            }
+            
         }
+
     }
 }
--- PokeAPISevenDaysOfCode/Model/Pokemon.cs
+++ PokeAPISevenDaysOfCode/Model/Pokemon.cs
@@ -12,5 +12,75 @@
 
         public string Url { get; set; }
 
+        private int _humor = 10;
+
+        public int Humor 
+        { 
+            get => _humor;
+            set
+            {
+                if (value < 0)
+                    _humor = 0;
+                else if (value > 10)
+                    _humor = 10;
+                else
+                    _humor = value;
+            }
+        }
+
+        public string HumorDescricao => _humor > 5 ? "FELIZ" : "TRISTE";
+
+        private int _alimentacao = 10;
+
+        public int Alimentacao {
+            get => _alimentacao;
+            set
+            {
+                if (value < 0)
+                    _alimentacao = 0;
+                else if (value > 10)
+                    _alimentacao = 10;
+                else
+                    _alimentacao = value;
+            } 
+        }
+
+        public string AlimentacaoDescricao => _alimentacao > 5 ? "ALIMENTADO" : "FAMINTO";
+
+        public Pokemon() { }
+
+        public Pokemon(string name, int height, int weight, List<AbilitiesList> abilities)
+        {
+            Name = name;
+            Height = height;
+            Weight = weight;
+            Abilities = abilities;
+        }
+
+        public void Brincar()
+        {
+            Humor++;
+            Alimentacao--;
+        }
+
+        public void Alimentar()
+        {
+            Alimentacao++;
+        }
+
+        public void StatusDoPokemon()
+        {
+            Console.WriteLine($"NOME DO POKEMON: {Name.ToUpper()}");
+            Console.WriteLine($"ALTURA: {Height}");
+            Console.WriteLine($"PESO: {Weight}");
+            Console.WriteLine($"{Name.ToUpper()} ESTA {AlimentacaoDescricao.ToUpper()}");
+            Console.WriteLine($"{Name.ToUpper()} ESTA {HumorDescricao.ToUpper()}");
+            Console.WriteLine("HABILIDADES: ");
+            foreach (var ability in Abilities)
+            {
+                Console.WriteLine($"{ability.Ability.Name}");
+            }
+        }
+
     }
 }
